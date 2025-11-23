77ad79c124e14880f7bf29f2f2e2a33dc7e8692f
Martin Koller <martin.koller@betsys.com>
2025-11-23 21:31:43 +0100

    Revert "feat: move form to a customer-filter-content"
    This reverts commit e6bb96fb50f146f1c7d998b4c09d6cfba9d9631f.

--- src/app/features/customer-filter/components/customer-filter-content/customer-filter-content.component.ts
+++ src/app/features/customer-filter/components/customer-filter-content/customer-filter-content.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
 import { CustomerEvent } from '../../../../core/models/customer-events';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CustomerFilterStepComponent } from '../customer-filter-step/customer-filter-step.component';
 import { SeparatorComponent } from '../../../../shared/separator/separator.component';
 import { CustomerFilterForm } from '../../../../core/models/customer-filter.form';
@@ -21,55 +21,18 @@ import { ButtonComponent } from '../../../../shared/button/button.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CustomerFilterContentComponent {
-  private formBuilder = inject(FormBuilder);
-
   customerEvents = input.required<CustomerEvent[]>();
+  customerFilterForm = input.required<FormGroup<CustomerFilterForm>>();
+  discardFiltersTrigger = input<number>();
+
+  addStep = output();
+  removeStep = output<number>();
+  discardFilters = output();
 
   readonly addFunnelStepKey = '+ Add funnel step';
   readonly heading = 'customer filter';
 
-  customerFilterForm: FormGroup<CustomerFilterForm> = this.formBuilder.group({
-    steps: this.formBuilder.array([
-      this.formBuilder.group({
-        event: this.formBuilder.nonNullable.control(''),
-        attributes: this.formBuilder.array<FormControl<string | null>>([]),
-      }),
-    ]),
-  });
-
-  constructor() {
-    this.customerFilterForm.valueChanges.subscribe((value) => {
-      console.log('Form value changed', value);
-    });
-    this.steps.valueChanges.subscribe((value) => {
-      console.log('Steps changed:', value);
-    });
-  }
-
   get steps() {
-    return this.customerFilterForm.controls.steps;
-  }
-
-  addStep() {
-    const step = this.formBuilder.group({
-      event: this.formBuilder.nonNullable.control(''),
-      attributes: this.formBuilder.array<FormControl<string | null>>([]),
-    });
-
-    this.steps.push(step);
-  }
-
-  removeStep(index: number) {
-    this.steps.removeAt(index);
-  }
-
-  discardFilters() {
-    this.steps.clear({ emitEvent: false });
-    this.steps.push(
-      this.formBuilder.group({
-        event: this.formBuilder.nonNullable.control(''),
-        attributes: this.formBuilder.array<FormControl<string | null>>([]),
-      }),
-    );
+    return this.customerFilterForm().controls.steps;
   }
 }
--- src/app/features/customer-filter/customer-filter.component.ts
+++ src/app/features/customer-filter/customer-filter.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
 import { FilterEventsService } from '../../core/services/filter-events.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { CustomerFilterContentComponent } from './components/customer-filter-content/customer-filter-content.component';
@@ -12,6 +13,52 @@ import { CustomerFilterContentComponent } from './components/customer-filter-con
 })
 export class CustomerFilterComponent {
   private filterEventsService = inject(FilterEventsService);
+  private formBuilder = inject(FormBuilder);
 
   filterEvents = toSignal(this.filterEventsService.getCustomerEvents());
+
+  customerFilterForm = this.formBuilder.group({
+    steps: this.formBuilder.array([
+      this.formBuilder.group({
+        event: this.formBuilder.nonNullable.control(''),
+        attributes: this.formBuilder.array<FormControl<string | null>>([]),
+      }),
+    ]),
+  });
+
+  constructor() {
+    this.customerFilterForm.valueChanges.subscribe((value) => {
+      console.log('Form value changed', value);
+    });
+    this.steps.valueChanges.subscribe((value) => {
+      console.log('Steps changed:', value);
+    });
+  }
+
+  get steps() {
+    return this.customerFilterForm.controls.steps;
+  }
+
+  addStep() {
+    const step = this.formBuilder.group({
+      event: this.formBuilder.nonNullable.control(''),
+      attributes: this.formBuilder.array<FormControl<string | null>>([]),
+    });
+
+    this.steps.push(step);
+  }
+
+  removeStep(index: number) {
+    this.steps.removeAt(index);
+  }
+
+  discardFilters() {
+    this.steps.clear({ emitEvent: false });
+    this.steps.push(
+      this.formBuilder.group({
+        event: this.formBuilder.nonNullable.control(''),
+        attributes: this.formBuilder.array<FormControl<string | null>>([]),
+      }),
+    );
+  }
 }
