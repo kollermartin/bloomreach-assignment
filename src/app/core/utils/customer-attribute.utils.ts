fb775a54558edba8af4f15b41a3b31afb0629463
Martin Koller <martin.koller@betsys.com>
2025-11-24 18:23:51 +0100

    refactor: fix tests + add few test cases

--- src/app/core/utils/customer-attribute.utils.ts
+++ src/app/core/utils/customer-attribute.utils.ts
@@ -1,12 +1,16 @@
 import { CustomerEventProperty } from '../models/customer-events.model';
 
-export function buildAttributeListMap(attributeListOptions?: CustomerEventProperty[]): Record<string, CustomerEventProperty> {
+export function buildAttributeListMap(
+  attributeListOptions?: CustomerEventProperty[],
+): Record<string, CustomerEventProperty> {
   if (!attributeListOptions) {
     return {};
   }
-  return attributeListOptions.reduce((acc, curr) => {
-    acc[curr.property] = curr;
-    return acc;
-  }, {} as Record<string, CustomerEventProperty>);
+  return attributeListOptions.reduce(
+    (acc, curr) => {
+      acc[curr.property] = curr;
+      return acc;
+    },
+    {} as Record<string, CustomerEventProperty>,
+  );
 }
-
